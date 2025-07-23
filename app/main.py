a17152514b57fc348f74a7cee738ee24fefca367
shuki <shukiheftler@gmail.com>
2025-07-23 23:14:14 +0300

    v1_classifiar

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+
+from prediction.fetch_model_data.fetch_model import GetModel
+from prediction.predict.predict import Predict
 from model_manager import ModelManager
 import os
 import uvicorn
@@ -11,7 +14,7 @@ def get_model_path(file_base):
         return f"/app/data/{file_base}"
     else:
 
-        return f"data/{file_base}"
+        return f"/app/data/{file_base}"
 # file = 'app/data/buy_computer.csv'
 file = get_model_path('buy_computer.csv')
 label = 'buys_computer'
@@ -35,6 +38,28 @@ def evaluation():
 @app.get('/confusion_matrix')
 def confusion():
     return model_manager.get_confusion_matrix()
+
+fetch = GetModel()
+
+@app.post('/prediction')
+def prediction(row:dict):
+    print(">>> קיבלנו נתונים לתחזית:")
+    print(row)
+
+    if fetch.trained_model is None:
+        print("❌ המודל לא נטען כמו שצריך!")
+        return {"error": "Model not loaded"}
+
+    try:
+        predict = Predict(fetch.trained_model,fetch.class_prob,fetch.labels)
+        res = predict.predict_row(row)
+        print(">>> תחזית התקבלה בהצלחה:", res)
+        return {'prediction':res}
+    except Exception as e:
+        print("❌ שגיאה בזמן תחזית:", e)
+        return {"error": str(e)}
+
+
 
 if __name__ == "__main__":
 
--- app/prediction/predict/predict.py
+++ app/prediction/predict/predict.py
@@ -6,17 +6,22 @@ class Predict:
         self.model = model
         self.labels = labels
         self.class_prob = class_prob
+
     def predict_row(self, row: dict) -> str:
+        print(">> התחזית מתחילה")
+        print(">> קלט:", row)
         scores = {}
         for label in self.labels:
             log_prob = 0
             for feature, value in row.items():
                 prob = self.model.get(feature, {}).get(value, {}).get(label, 1e-9)
                 log_prob += math.log(prob)
-            print(self.class_prob[label])
-            print(type(self.class_prob[label]))
-
+            print(f"label={label}, class_prob={self.class_prob.get(label)}, type={type(self.class_prob.get(label))}")
             log_prob += math.log(self.class_prob[label])
             scores[label] = math.exp(log_prob)
+        print(">> scores:", scores)
+        if not scores:
+            return "Error: Empty scores"
         return max(scores, key=scores.get)
+
 
